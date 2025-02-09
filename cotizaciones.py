2bf55fa56c36f722ccf21b39095eb796d432a384
VictorNomadCoder <victorprogbcn@gmail.com>
2025-02-09 17:48:17 +0100

    Update cotizaciones.py

--- cotizaciones.py
+++ cotizaciones.py
@@ -20,5 +20,6 @@ def obtener_cotizacion():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
